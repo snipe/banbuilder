fd4b3fef9e15cfd342336124936e1e2995d8c3c8
snipe <snipe@snipe.net>
2014-04-01 17:27:27 -0400

    Updated include for language-specific

--- example.php
+++ example.php
@@ -1,18 +1,18 @@
 <?php
 /**
-* This is a simple script to test whether the words you've 
+* This is a simple script to test whether the words you've
 * added will be properly censored in a sentence format.
-* 
+*
 * Usage via command line: /path/to/php example.php "snipe is a bitch, but she knows her shit. NO BULLSHIT."
 * Usage via www: /url/example.php?snipe is a bitch but she's fucking great! NO BULLSHIT.
-* 
+*
 * Output:
-* ORIGINAL:      snipe is a bitch, but she knows her shit. NO BULLSH!T. 
+* ORIGINAL:      snipe is a bitch, but she knows her shit. NO BULLSH!T.
 * SUBSITUTIONS:  snipe is a bitch, but she knows her shit. NO BULLSHiT.
 * PROCESSED:     snipe is a *****, but she knows her ****. NO BULL****.
 */
 
-include('wordlist-regex.php');
+include('en.wordlist-regex.php');
 include('censor.function.php');
 
 // cli or www?
@@ -26,10 +26,9 @@ if (isset($argv)) {
 	header('Content-Type: text/plain');
 }
 
-$censored = censorString($input, $badwords);			
+$censored = censorString($input, $badwords);
 echo "\nPURE: ".$input."\nORIGINAL:      ".$censored['orig']."\nPROCESSED:     ".$censored['clean']."\n\n";
- 
 
 
 
-	
+
