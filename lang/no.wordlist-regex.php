a0350a998e0ae8b82d42ecff80c3de49fd6d08f0
Alex Jegtnes <saccosekk@gmail.com>
2014-04-02 12:36:00 +0100

    Adding Norwegian attribution

--- lang/no.wordlist-regex.php
+++ lang/no.wordlist-regex.php
@@ -1,6 +1,10 @@
 <?php
 /**
  * PHP Array for Norwegian words
+ * Delightful sweary additions from:
+ * - http://www.youswear.com/index.asp?language=Norwegian
+ * - http://www.insults.net/html/swear/norwegian.html
+ * - http://www.reddit.com/r/europe/comments/1hzv9d/tell_me_the_best_swearwords_in_your_language/cazmerv
  */
 
 // General insults and rudeness
