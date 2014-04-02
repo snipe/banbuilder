404e74b7f8ec444f20fa81716be79b9fb5fa1d84
snipe <snipe@snipe.net>
2014-04-01 20:18:51 -0400

    Added definitions

--- es.wordlist-regex.php
+++ es.wordlist-regex.php
@@ -1,63 +1,64 @@
 <?php
 /**
- * PHP Array
+ * PHP Array for Spanish words
+ * Original word list from http://bulbapedia.bulbagarden.net/wiki/List_of_censored_words_in_Generation_V
  */
 
-$badwords = array(
-		'bollera',
-		'cabron',
-		'cabrón',
-		'cabrona',
-		'cabronazo',
-		'capulla',
-		'capullo',
-		'chichi',
-		'chocho',
-		'cojon',
-		'cojón',
-		'cojones',
-		'comepollas',
-		'cono',
-		'coño',
-		'culo',
-		'follar',
-		'follen',
-		'furcia',
-		'gilipollas',
-		'hijaputa',
-		'hijo puta',
-		'hijoputa',
-		'hostia',
-		'joder',
-		'jodete',
-		'jódete',
-		'joputa',
-		'mamada',
-		'mamon',
-		'mamón',
-		'mamona',
-		'marica',
-		'maricon',
-		'maricón',
-		'maricona',
-		'mariconazo',
-		'nazi',
-		'ojete',
-		'ostia',
-		'pajillero',
-		'pendon',
-		'pendón',
-		'picha',
-		'polla',
-		'pollon',
-		'pollón',
-		'polvo',
-		'potorro',
-		'puta',
-		'puto',
-		'puton',
-		'putón',
-		'tortillera',
-		'zorron',
-		'zorrón'
+array_push($badwords,
+		'bollera',		// Lesbian
+		'cabron',		// Dickhead
+		'cabrón',		// Dickhead
+		'cabrona',		// Dickhead (feminine)
+		'cabronazo',	// Bastard
+		'capulla',		// Asshole (feminine)
+		'capullo',		// Asshole
+		'chichi',		// Pussy
+		'chocho',		// Pussy
+		'cojon',		// Testicle (slang)
+		'cojón',		// Testicle (slang)
+		'cojones',		// Testicles (slang)
+		'comepollas',	// Cocksucker
+		'cono',			// Cunt
+		'coño',			// Cunt
+		'culo',			// Ass
+		'follar',		// Fuck
+		'follen',		// Go fuck
+		'furcia',		// Whore
+		'gilipollas',	// Jerk
+		'hijaputa',		// Son of a bitch (feminine)
+		'hijo puta',	// Son of a bitch
+		'hijoputa',		// Son of a bitch
+		'hostia',		// Bastard, shit, to hit
+		'joder',		// Fuck
+		'jodete',		// Go fuck yourself
+		'jódete',		// Go fuck yourself
+		'joputa',		// Abbreviation of "hijo de puta" (son of a bitch)
+		'mamada',		// Blowjob
+		'mamon',		// Sucker
+		'mamón',		// Sucker
+		'mamona',		// Sucker (feminine)
+		'marica',		// Faggy
+		'maricon',		// Faggot
+		'maricón',		// Faggot
+		'maricona',		// Drag queen, lesbian
+		'mariconazo',	// Faggot
+		'nazi',			// Nazi
+		'ojete',		// Anus (slang)
+		'ostia',		// Abbreviation of "hostia" (bastard, shit, to hit)
+		'pajillero',	// Wanker
+		'pendon',		// Slob
+		'pendón',		// Slob
+		'picha',		// Dick
+		'polla',		// Dick
+		'pollon',		// Dick
+		'pollón',		// Dick
+		'polvo',		// To ejaculate
+		'potorro',		// Vagina
+		'puta',			// Bitch/whore
+		'puto',			// Bitch/whore (masculine), faggot
+		'puton',		// Bitch/whore
+		'putón',		// Bitch/whore
+		'tortillera',	// Lesbian
+		'zorron',		// To be/act slutty
+		'zorrón'		// To be/act slutty
 );
