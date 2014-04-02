b682e85706fc2938ef37f2908bff3d694b361206
snipe <snipe@snipe.net>
2014-04-02 00:41:46 -0400

    Japanese dictionary file

---
+++ jp.wordlist-regex.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * PHP Array for Janapese words
+ * Original word list from http://bulbapedia.bulbagarden.net/wiki/List_of_censored_words_in_Generation_V
+ */
+
+array_push($badwords,
+		'あいえき',     	// Sexual fluids
+		'せいえき',     	// Semen
+		'ファック',     	// Fuck
+		'いぬごろし',   	// Dog killer
+		'いんぱい',     	// Slut
+		'いんもう',     	// Pubic hair
+		'うんこ',     	// Shit
+		'うんち',     	// Shit
+		'おめこ',     	// Vagina
+		'かたわ',     	// Cripple
+		'きちがい',     	// Lunatic
+		'くろんぼ',     	// Negro
+		'けとう',     	// Hairy foreigner
+		'ころす',     	// Kill
+		'ごうかん',     	// Rape
+		'さんごくじん',	// Foreign resident
+		'しなじん',    	// Chinese person (offensive)
+		'しね',     		// Die
+		'せっくす',    	// Sex
+		'ちゃんころ',    // Chink
+		'ちんこ',     	// Penis
+		'ちんちん',     	// Dick
+		'ちんぽ',     	// Dick
+		'ちんば',     	// Cripple
+		'つんぼ',     	// Deaf person
+		'どかた',     	// Laborer
+		'とさつ',     	// Slaughter
+		'どもり',     	// Speech impediment
+		'にぐろ',     	// Negro
+		'にんぴにん',  	// Brute
+		'びっこ',     	// Cripple
+		'ひにん',     	// Contraception / Non-human (insult to Burakumin)
+		'ふぇら',     	// Fellatio
+		'ぶらく'			// Buraku
+		'ぺにす',     	// Penis
+		'まんこ',     	// Vagina
+		'おまんこ',     	// Vagina
+		'めくら',     	// Blind person, illiterate person
+		'やらせろ',    	// "Let me do you"
+		'やりまん',    	// Slut
+		'りょうじょく', 	// Rape
+		'れいぷ',     	// Rape
+		'ろりこん',  	// Pedophile
+);
