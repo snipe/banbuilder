c2174dc27c5954d34a1b9003dcc53e985a2534d2
rengetsu <rengetsu@mail.ru>
2019-07-15 16:28:29 +0200

    Russian words

---
+++ src/dict/ru.php
@@ -0,0 +1,15 @@
+<?php
+/**
+  * PHP Russian Bad Words
+  * Original source - http://www.russki-mat.net/e/mat_slovar.htm
+  */
+array_push($badwords,
+    'бля',			//
+	'блять',		//
+    'хуй',			//
+    'пизда',		//
+    'ебать',		//
+    'дебил',		//
+    'дурак',		//
+    'идиот'			//
+);
