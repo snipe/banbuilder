882ec3fc9fe4d1b6c533ef54653385a455ade81d
rengetsu <rengetsu@mail.ru>
2019-07-15 16:42:09 +0200

    new version of russian words

--- src/dict/ru.php
+++ src/dict/ru.php
@@ -4,12 +4,16 @@
   * Original source - http://www.russki-mat.net/e/mat_slovar.htm
   */
 array_push($badwords,
-    'бля',			//
-	'блять',		//
-    'хуй',			//
-    'пизда',		//
-    'ебать',		//
-    'дебил',		//
-    'дурак',		//
-    'идиот'			//
+	'бля',		//	fucking
+	'блять',	//	fuck
+	'хуй',		//	dick
+	'пизда',	//	pussy
+	'ебать',	//	fuck
+	'дебил',	//	moron
+	'дурак',	//	fool
+	'идиот',	//	idiot
+	'ебальник',	// 	snout nose
+	'пиздец',	// 	fucked up
+	'пиздешь',	// 	bullshit
+	'дерьмо'	// 	shit
 );
